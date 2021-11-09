558bb9fb402bbebaf3e109d02627b9cef1eb2bb0
pamod1998 <pamod145@gmail.com>
2021-11-09 21:30:04 +0530

    Add home,header.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -161,6 +161,17 @@ const Google = styled.button`
     border-radius: 28px;
     box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
     inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+
+    vertical-align: middle;
+    z-index: 0;
+    transition-duration: 167ms;
+    font-size:20px;
+    color: rgba(0, 0, 0, 0.6);
+
+    &:hover {
+        background-color: rgba(207, 207, 207, 0.25);
+        color: rgba(0, 0, 0, 0.75);
+    }
 `
 
 
