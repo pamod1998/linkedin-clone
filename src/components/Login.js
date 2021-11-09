7c661f9a280cc06bd99cb41d8bf1ca5de7423e4d
pamod1998 <pamod145@gmail.com>
2021-11-09 16:22:33 +0530

    ADD

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,18 @@ const Login = (props) => {
                     <SignIn>Sign In</SignIn>
                 </div>
             </Nav>
-            <Section>section</Section>
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src="/images/login-hero.svg" alt="" />
+                </Hero>
+                <Form>
+                    <Google>
+                        <img src="/images/google.svg" alt="" />
+                        Sign in with Google
+                    </Google>
+                </Form>
+            </Section>
         </Container>
     )
 }
@@ -43,6 +54,7 @@ const Nav = styled.div`
 
 
 const Join =styled.a`
+    cursor: pointer;
     font-size: 16px;
     padding: 10px 12px;
     text-decoration: none;
@@ -58,6 +70,7 @@ const Join =styled.a`
 `;
 
 const SignIn = styled.a`
+    cursor: pointer;
     box-shadow: inset 0 0 0 1px #0a66c2;
     color: #0a66c2;
     border-radius: 24px;
@@ -78,8 +91,77 @@ const SignIn = styled.a`
 `
 
 const Section = styled.section`
-
+    display: flex;
+    align-content: start;
+    min-height: 700px;
+    padding-bottom: 138px;
+    padding-top:40px;
+    padding: 60px 0;
+    position: relative;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 1128px;
+    align-items: center;
+    margin: auto;
+    @media (max-width: 768px) {
+        margin: auto;
+        min-height: 0px;
+    }
 
 `
+
+const Hero = styled.div`
+    width: 100%;
+    h1{
+        padding-bottom: 0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px) {
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }
+
+    img {
+
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
+    }
+
+`
+const Form = styled.div`
+    margin-top: 100px;
+    width: 400px;
+    @media (max-width: 768px){
+        margin-top: 20px;
+    }
+`
+
+const Google = styled.button`
+    display: flex;
+    justify-content: center;
+    background-color: #fff;
+    align-items: center;
+    height: 56px;
+    width: 100%;
+    border-radius: 28px;
+    box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+`
+
 
 export default Login;
