4761e487d09be9f3439c583eb40deb068f23b962
pamod1998 <pamod145@gmail.com>
2021-11-14 01:32:04 +0530

    Add

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,12 +19,59 @@ const Header = (props) => {
                 </Search>
                 <Nav>
                     <NavListWrap>
-                        <NavList>
-                            <a>
+                        <NavList className="active">
+                            <a href>
                                 <img src="/images/nav-home.svg" alt="" />
                                 <span>Home</span>
                             </a>
                         </NavList>
+                        <NavList>
+                            <a href>
+                                <img src="/images/nav-network.svg" alt="" />
+                                <span>My Network</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href>
+                                <img src="/images/nav-jobs.svg" alt="" />
+                                <span>Jobs</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href>
+                                <img src="/images/nav-messaging.svg" alt="" />
+                                <span>Messaging</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a href>
+                                <img src="/images/nav-notifications.svg" alt="" />
+                                <span>Notifications</span>
+                            </a>
+                        </NavList>
+
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt="" />
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg" alt="" />
+                            </a>
+
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt="" />
+                                <span>
+                                    Work
+                                    <img src="/images/down-icon.svg" alt="" />
+                                </span>
+                            </a>
+                        </Work>
+
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -39,7 +86,7 @@ const Container = styled.div`
     padding: 0 24px;
     position: fixed;
     top: 0;
-    width: 100%;
+    width: 100vw;
     z-index:100;
 
 `
@@ -47,9 +94,9 @@ const Container = styled.div`
 const Content = styled.div`
     display: flex;
     align-items: center;
-    margin: 0;
+    margin: 0 auto;
     min-height: 100%;
-    max-height: 112px;
+    max-width: 1128px;
 
 `
 const Logo = styled.span`
@@ -62,21 +109,21 @@ const Search = styled.div`
     opacity: 1;
     flex-grow: 1;
     position: relative;
-    &>div{
+    & > div{
         max-width: 280px;
         input{
             border: none;
             box-shadow: none;
             background-color: #eef3f8;
             border-radius:2px;
-            color: rgba(0,0,0,0.9);
+            color: rgba(0, 0, 0, 0.9);
             width: 218px;
             padding: 0 8px 0 40px;
             line-height: 1.75;
-            font-weight: 100;
-            font-size:14px;
+            font-weight: 400;
+            font-size: 14px;
             height: 34px;
-            border-color: #d4e2fa;
+            border-color: #dce6f1;
             vertical-align: text-top;
         }
     }
@@ -113,6 +160,20 @@ const NavListWrap = styled.ul`
     display: flex;
     flex-wrap: nowrap;
     list-style-type: none;
+
+    .active{
+        span:after {
+            content: "";
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #fff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            transition: transform 0.2s ease-in-out;
+            width: 100%;
+            border-color: rgba(0, 0, 0, 0.9);
+        }
+    }
 `
 
 const NavList = styled.li`
@@ -121,6 +182,7 @@ const NavList = styled.li`
     a {
         align-items: center;
         background: transparent;
+        display: flex;
         flex-direction: column;
         font-size: 12px;
         font-weight: 400;
@@ -130,13 +192,77 @@ const NavList = styled.li`
         min-width: 80px;
         position: relative;
         text-decoration: none;
+
         span{
             color: rgba(0, 0, 0, 0.6);
             display: flex;
             align-items: center;
 
         }
+
+        @media (max-width: 768px){
+            min-width: 70px;
+        }
+    }
+
+    &:hover, &:active{
+        a{
+            span{
+                color: rgba(0, 0, 0, 0.9);
+            }
+        }
     }
 
 `
+
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+
+`
+
+const User = styled(NavList)`
+    a > svg {
+        width: 24px;
+        border-radius: 50%;
+    }
+
+    a > img {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+
+    span {
+        display: flex;
+        align-items: center;
+    }
+
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+
+`
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0, 0 , 0, 0.08);
+
+`
+
+
+
+
+
 export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const Main = (props) => {
+  return (
+    <Container>
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+`;
+export default Main;
